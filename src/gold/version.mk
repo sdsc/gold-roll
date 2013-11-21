b6960558e94c5a51329dc2122b7f823ec8012c66
Trevor Cooper <tcooper@sdsc.edu>
2013-11-20 17:12:25 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/gold/version.mk
+++ src/gold/version.mk
@@ -1,3 +1,14 @@
-NAME    = gold
-VERSION = 2.2.0.5
-RELEASE = 1
+NAME               = gold
+VERSION            = 2.2.0.5
+RELEASE            = 1
+PKGROOT            = /opt/gold
+
+SRC_SUBDIR         = gold
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
